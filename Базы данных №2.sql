8d2db5f547531c200ca6b21ffd997232edfc4766
Polina Naumovich <NaumovichP@yandex.ru>
2024-02-15 12:43:35 +0300

    Add files sql-db

---
+++ Базы данных №2.sql
@@ -0,0 +1,39 @@
+CREATE TABLE IF NOT EXISTS genres(
+	id SERIAL primary key,
+	genres_name VARCHAR(40) not null
+);
+CREATE TABLE IF NOT EXISTS artists(
+	id SERIAL primary key,
+	nickname VARCHAR(40) not null
+);
+CREATE TABLE IF NOT EXISTS artistsgenres(
+	id SERIAL primary key,
+	artists_id INTEGER not null references artists(id),
+	genres_id INTEGER not null references genres(id)
+);
+CREATE TABLE IF NOT EXISTS albums(
+	id SERIAL primary key,
+	albums_name VARCHAR(40) not null,
+	release_date DATE not null
+);
+CREATE TABLE IF NOT EXISTS artistsalbums(
+	id SERIAL primary key,
+	artists_id INTEGER not null references artists(id),
+	albums_id INTEGER not null references albums(id)
+);
+CREATE TABLE IF NOT EXISTS tracks(
+	id SERIAL primary key,
+	tracks_name VARCHAR(40) not null,
+	duration INTEGER not null,
+	albums_id INTEGER not null references albums(id)
+);
+CREATE TABLE IF NOT EXISTS collections(
+	id SERIAL primary key,
+	collections_name VARCHAR(40) not null,
+	release_date DATE not null
+);
+CREATE TABLE IF NOT EXISTS trackscollections(
+	id SERIAL primary key,
+	tracks_id INTEGER not null references tracks(id),
+	collections_id INTEGER not null references collections(id)
+);
